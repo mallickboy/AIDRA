f601cdf218b321accd197d7ddd50c9909e3111c8
MrAvishek <amondal.dev@gmail.com>
2025-04-14 14:49:02 +0530

    Initial commit

---
+++ structure.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path  # Correct module
+
+folder_str = [
+    "src/__init__.py",
+    "src/helper.py",
+    "requirements.txt",       # fixed typo
+    "setup.py",
+    "app.py",
+    "playground/test.ipynb"
+]
+
+for filepath in folder_str:
+    filepath = Path(filepath)
+    if filepath.parent != Path("."):
+        os.makedirs(filepath.parent, exist_ok=True)
+    if not filepath.exists():
+        filepath.touch()
